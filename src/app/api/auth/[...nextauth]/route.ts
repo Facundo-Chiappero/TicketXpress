5a755ebfd99ca2f0c5628ac8a84194fa5ade0345
FaAnCh <facundochiappero5@gmail.com>
2025-04-26 18:25:09 -0300

    modified:   src/app/api/auth/[...nextauth]/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
-import NextAuth, { AuthOptions } from 'next-auth'
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '@/lib/prisma'
 import { providers } from '@/lib/auth/providers'
 import { callbacks } from '@/lib/auth/callbacks'
 import { PAGES } from '@/constants/frontend/pages'
 
-export const authOptions: AuthOptions = {
+export const GET = (req, res) => NextAuth(req, res, {
   providers,
   callbacks,
   adapter: PrismaAdapter(prisma),
@@ -15,9 +16,16 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: 'jwt',
   },
-}
+})
 
-const handler = NextAuth(authOptions)
-
-export const GET = handler
-export const POST = handler
+export const POST = (req, res) => NextAuth(req, res, {
+  providers,
+  callbacks,
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: PAGES.AUTH.LOGIN,
+  },
+  session: {
+    strategy: 'jwt',
+  },
+})
