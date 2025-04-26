10ee2018ff76b52dc277400d18581de071030d98
FaAnCh <facundochiappero5@gmail.com>
2025-04-26 19:10:42 -0300

    minor styles change

--- src/app/ui/user/UserEvents.tsx
+++ src/app/ui/user/UserEvents.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function UserEvents({ user, futureEvents }: Props) {
   return (
-    <section className="flex flex-col mt-4 justify-center items-center">
+    <section className="flex flex-col mt-4 justify-center items-center gap-4">
       <h2 className="w-[80%] text-4xl font-bold mb-2">{USER_EVENTS.TITLE}</h2>
       {futureEvents.length > 0 ? (
         futureEvents.map((e) => {
